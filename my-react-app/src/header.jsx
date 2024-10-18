ee54e29cd83d94f5b669b5969f15a100341e079b
Asingura Philip <asinguraphilip@gmail.com>
2024-10-18 23:43:57 +0300

    nav bar

--- my-react-app/src/header.jsx
+++ my-react-app/src/header.jsx
@@ -4,6 +4,14 @@ function Header(){
             <h1>
                 My website
             </h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
         </header>
     );
 }
